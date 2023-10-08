9e4aa9709b993a95fcfa0da6e633bc9976eebcf7
1minmin1 <95240414+1minmin1@users.noreply.github.com>
2023-10-08 22:05:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace L
+{
+    class Program
+    {
+        /// <summary>
+        /// Старт
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.Write("Длина: ");
+
+            double a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ширина: ");
+
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Высота: ");
+
+            double c = Convert.ToInt32(Console.ReadLine());
+
+            var Volume = VolumeCalculator.GetVolume(a, b, c);
+
+            Console.WriteLine(Volume);
+
+            Console.WriteLine(VolumeDescription.GetVolumeDescription(Volume));
+        }
+    }
+}
---
+++ ValidationHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace L
+{
+    public static class ValidationHelper
+    {
+        /// <summary>
+        /// Проверка на минимальное значение
+        /// </summary>
+        /// <param name="value">Число для проверки</param>
+        /// <param name="MinValue">Минимальное значение</param>
+        /// <param name="message"></param>
+        /// <exception cref="ArgumentOutOfRangeException">Если значение меньше или равно минимальному значению, выводится сообщение</exception>
+        public static void CheckRangeValue(double value, int MinValue, string message)
+        {
+            if (value <= MinValue)
+            {
+
+                throw new ArgumentOutOfRangeException(nameof(value),
+
+                string.Format(message, MinValue));
+            }
+        }
+    }
+}
+
